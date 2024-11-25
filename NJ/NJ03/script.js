5368e00fac4fdae559f20f7d34a645ca4033d9a5
rajgupta2399 <rajkumardotnet2499@gmail.com>
2024-11-25 12:06:08 +0530

    Hoisting

---
+++ NJ/NJ03/script.js
@@ -0,0 +1,18 @@
+var x = 7;
+function getName() {
+  console.log("Namaste Javascript");
+}
+
+console.log(x); // 7
+getName(); // Namaste Javascript
+
+
+// See Another Case
+
+getName1();                         // Namaste Js
+console.log(y);                     // undefined
+
+var y = 11;
+function getName1() {
+  console.log("Namaste Javascript");
+}
